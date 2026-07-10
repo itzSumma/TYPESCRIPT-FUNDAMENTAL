14b66481c71501fd80e52fdebcf3a4de4e60ca37
itzSumma <itzSumma11@gmail.com>
2026-07-10 22:29:15 +0600

    Complete TypeScript primitive types practice

--- src/01-types.ts
+++ src/01-types.ts
@@ -1,9 +1,31 @@
 // Primitive Types
 
 let firstName: string = "Sharmin";
-let age: number = 22;
+// let age: number = 22;
 let isDeveloper: boolean = true;
 
 console.log(firstName);
-console.log(age);
+// console.log(age);
 console.log(isDeveloper);
+
+let age: number = 22;
+let salary: number = 30000;
+let cgpa: number = 3.85;
+
+console.log(age);
+console.log(salary);
+console.log(cgpa);
+
+salary = 35000;
+
+console.log(salary);
+
+
+
+/**number    → সংখ্যা
+string    → লেখা
+boolean   → true / false
+null      → ইচ্ছা করে খালি
+undefined → এখনো value নেই
+symbol    → unique value
+bigint    → খুব বড় সংখ্যা */
