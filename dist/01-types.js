fc2462a8d0d7f8b294d8cc6ff0e30430dab8c5e2
itzSumma <itzSumma11@gmail.com>
2026-07-10 21:11:42 +0600

    initial typescript fundamentals project

---
+++ dist/01-types.js
@@ -0,0 +1,10 @@
+"use strict";
+// Primitive Types
+Object.defineProperty(exports, "__esModule", { value: true });
+let firstName = "Sharmin";
+let age = 22;
+let isDeveloper = true;
+console.log(firstName);
+console.log(age);
+console.log(isDeveloper);
+//# sourceMappingURL=01-types.js.map
---
+++ src/01-types.ts
@@ -0,0 +1,9 @@
+// Primitive Types
+
+let firstName: string = "Sharmin";
+let age: number = 22;
+let isDeveloper: boolean = true;
+
+console.log(firstName);
+console.log(age);
+console.log(isDeveloper);
