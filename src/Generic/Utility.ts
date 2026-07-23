249acfc8297f3d3d89b23517c210b809e20909fa
itzSumma <itzSumma11@gmail.com>
2026-07-23 21:07:29 +0600

    feat: add Product2 interface and showProductCard function for displaying product details

--- src/Generic/Utility.ts
+++ src/Generic/Utility.ts
@@ -16,3 +16,21 @@ function updateProduct(id: number, updatedData: Partial<Product>) {
   console.log(`Updating product ${id} with:`, updatedData);
 }
 
+interface Product2 {
+  id: number;
+  title: string;
+  price: number;
+  stock: number;
+}
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  stock: number;
+}
+
+function showProductCard(product: Pick<Product, "title" | "price">) {
+  console.log(`${product.title} - ${product.price} taka`);
+}
+
+showProductCard({ title: "Rose Bouquet", price: 500 }); 
